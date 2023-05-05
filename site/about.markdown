c97999f09a6beda5c0cfa9660b6c02b6a632f444
Yoo Chung <dev@chungyc.org>
2023-05-04 20:48:40 -0400

    Set up some more rules and static pages.

--- site/about.markdown
+++ site/about.markdown
@@ -1,13 +1,17 @@
 ---
 title: About
-published: 2008-09-03T14:08:49Z
-categories: 
-tags: 
+published: 2008-09-03
 ---
 
-<cite>Stochastic Scribbles</cite> is a blog maintained by <a href="https://chungyc.org/" rel="me">Yoo</a>, where he talks about anything non-personal that catches his fancy.  Given his interest in the physical sciences, he often talks about astronomy and the exploration of space.  He is also interested in physics.
+Stochastic Scribbles is a blog maintained by [Yoo Chung](https://chungyc.org),
+where he talks about anything non-personal that catches his fancy.
 
-Everything that is written in Stochastic Scribbles is solely from the viewpoint of the author.  None of the content should be interpreted as being presented on the behalf of any other individual or organization.  And as the author is only human, there is no guarantee as to the accuracy of the content, even though the author tries to avoid factual errors.
+Everything that is written in Stochastic Scribbles is solely from the viewpoint of the author.
+None of the content should be interpreted as being presented
+on the behalf of any other individual or organization.
+And as the author is only human,
+there is no guarantee as to the accuracy of the content,
+even though the author tries to avoid factual errors.
 
-This blog is powered by <a href="https://wordpress.org/">WordPress</a>.  Hosting for this blog is provided by <a href="https://dreamhost.com/">Dreamhost</a>.
-
+This blog is powered by [Hakyll](https://jaspervdj.be/hakyll/).
+Hosting for this blog is provided by [Dreamhost](https://dreamhost.com/)
--- src/Rules.hs
+++ src/Rules.hs
@@ -1,9 +1,8 @@
-{- |
-Description: Rules for generating the blog.
-Copyright: Copyright (C) 2023 Yoo Chung
-License: All rights reserved
-Maintainer: web@chungyc.org
--}
+-- |
+-- Description: Rules for generating the blog.
+-- Copyright: Copyright (C) 2023 Yoo Chung
+-- License: All rights reserved
+-- Maintainer: web@chungyc.org
 module Rules (rules) where
 
 import Hakyll
@@ -31,15 +30,16 @@ serverRules = do
 
 pageRules :: Rules ()
 pageRules = do
+  create ["index.html"] $ do
+    route idRoute
+    undefined
+
   match "about.markdown" $ do
     route $ constRoute "about/index.html"
-    compile $ pandocCompiler
-      >>= loadAndApplyTemplate undefined defaultContext
-
-  match "policies.markdown" $ do
-    route $ constRoute "policies/index.html"
-    compile $ pandocCompiler
-      >>= loadAndApplyTemplate undefined defaultContext
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate defaultTemplate defaultContext
+        >>= cleanupUrls
 
   create ["archives/index.html"] $ do
     route idRoute
@@ -47,7 +47,49 @@ pageRules = do
 
 postRules :: Rules ()
 postRules = do
-  match "posts/**.markdown" undefined
+  match "posts/**.markdown" $ do
+    route $ composeRoutes replacePrefix replaceSuffix
+    compile $
+      pandocCompiler
+        >>= saveSnapshot "posts"
+        >>= loadAndApplyTemplate defaultTemplate defaultContext
+        >>= loadAndApplyTemplate "templates/post.html" defaultContext
+        >>= cleanupUrls
+
+  create ["feed/index.xml"] $ do
+    route idRoute
+    compile $ do
+      let feedContext =
+            mconcat
+              [ teaserField "posts" "description",
+                bodyField "description",
+                defaultContext
+              ]
+      posts <- recentFirst =<< loadAllSnapshots "**" "posts"
+      renderRss feedConfiguration feedContext posts
+  where
+    replacePrefix = gsubRoute "^posts/" (const "")
+    replaceSuffix = gsubRoute "\\.markdown$" (const "/index.html")
+    feedConfiguration =
+      FeedConfiguration
+        { feedTitle = "Stochastic Scribbles",
+          feedDescription = "Posts from Stochastic Scribbles, a blog by Yoo Chung.",
+          feedAuthorName = "Yoo Chung",
+          feedAuthorEmail = "web@chungyc.org",
+          feedRoot = "https://blog.chungyc.org"
+        }
 
 sitemapRules :: Rules ()
-sitemapRules = undefined
+sitemapRules = do
+  create ["sitemap.xml"] $ do
+    route idRoute
+    undefined
+
+defaultTemplate :: Identifier
+defaultTemplate = "templates/default.html"
+
+cleanupUrls :: Item String -> Compiler (Item String)
+cleanupUrls = pure . fmap (withUrls cleanupUrl)
+
+cleanupUrl :: String -> String
+cleanupUrl = undefined
