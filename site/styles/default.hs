fc295d39878a2fa8dba8ecb7d287bddc5c1d8685
Yoo Chung <dev@chungyc.org>
2023-12-14 21:37:58 -0500

    Adjust size of iframe, not just surrounding div.

--- site/styles/default.hs
+++ site/styles/default.hs
@@ -112,9 +112,10 @@ postStyle = do
     sym2 padding (em 0) (em 2)
     border (em 0.5) inset $ setA 0.5 dodgerblue
 
-  div # ".embedded-video" ? do
+  div # ".embedded-video" ** star ? do
     textAlign center
     maxWidth $ pct 100
+    "aspect-ratio" -: "auto"
 
 figures :: Css
 figures = do
